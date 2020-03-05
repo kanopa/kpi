2c98298bd886aacc504c11e7c20c26d62c26151f
kanopa <konoplyanka1999@gmail.com>
2020-03-05 13:44:43 +0200

    Done

--- Tableware/app/src/main/java/com/example/tableware/ChooseActivity.kt
+++ Tableware/app/src/main/java/com/example/tableware/ChooseActivity.kt
@@ -1,10 +1,12 @@
 package com.example.tableware
 
+import android.content.Intent
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-
+import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_choose.*
+import kotlinx.android.synthetic.main.content_choose.*
 
 class ChooseActivity : AppCompatActivity() {
 
@@ -13,8 +15,27 @@ class ChooseActivity : AppCompatActivity() {
         setContentView(R.layout.activity_choose)
         setSupportActionBar(toolbar)
 
+        val intent = getIntent()
+        val message = intent.getStringExtra(MainActivity().EXTRA_MESSAGE)
+        val message2 = intent.getStringExtra(MainActivity().EXTRA_MESSAGE2)
+        val message3 = intent.getStringExtra(MainActivity().EXTRA_MESSAGE3)
+
+
+        val textView = findViewById<TextView>(R.id.Choose1)
+        val textView2 = findViewById<TextView>(R.id.Choose2)
+        val textView3 = findViewById<TextView>(R.id.Choose3)
+
+        textView.setText(message)
+        textView2.setText(message2)
+        textView3.setText(message3)
+
+        button_back.setOnClickListener {
+            val activityIntent = Intent(this, MainActivity::class.java)
+            startActivity(activityIntent)
+        }
+
         fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+            Snackbar.make(view, "Replace with your own $message", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
         }
     }
--- Tableware/app/src/main/java/com/example/tableware/MainActivity.kt
+++ Tableware/app/src/main/java/com/example/tableware/MainActivity.kt
@@ -12,6 +12,9 @@ import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.content_main.*
 
 class MainActivity : AppCompatActivity() {
+    val EXTRA_MESSAGE = "EXTRA_MESSAGE"
+    val EXTRA_MESSAGE2 = "EXTRA_MESSAGE2"
+    val EXTRA_MESSAGE3 = "EXTRA_MESSAGE3"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,16 +22,67 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(toolbar)
 
         val language = resources.getStringArray(R.array.Languages)
-
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, language)
         val spinner = findViewById<Spinner>(R.id.spinner)
+
         if(spinner != null)
         {
-            val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, language)
             spinner.adapter = adapter
         }
-
-        button.setOnClickListener { view ->
+        man1.setOnClickListener{
+            man2.setChecked(false)
+            man3.setChecked(false)
+        }
+        man2.setOnClickListener{
+            man1.setChecked(false)
+            man3.setChecked(false)
+        }
+        man3.setOnClickListener{
+            man1.setChecked(false)
+            man2.setChecked(false)
+        }
+        price1.setOnClickListener{
+            price2.setChecked(false)
+            price3.setChecked(false)
+        }
+        price2.setOnClickListener{
+            price1.setChecked(false)
+            price3.setChecked(false)
+        }
+        price3.setOnClickListener{
+            price1.setChecked(false)
+            price2.setChecked(false)
+        }
+        button.setOnClickListener {
             val activityIntent = Intent(this, ChooseActivity::class.java)
+            val spinnerTxt = spinner.selectedItem.toString()
+            val manufact1 = man1.getText().toString()
+            val manufact2 = man2.getText().toString()
+            val manufact3 = man3.getText().toString()
+            val priceText = price1.getText().toString()
+            val priceText2 = price2.getText().toString()
+            val priceText3 = price3.getText().toString()
+
+            if(man1.isChecked())
+            {
+                activityIntent.putExtra(EXTRA_MESSAGE2,manufact1)
+            } else if(man2.isChecked())
+            {
+                activityIntent.putExtra(EXTRA_MESSAGE2,manufact2)
+            } else{
+                activityIntent.putExtra(EXTRA_MESSAGE2,manufact3)
+            }
+            if( price1.isChecked())
+            {
+                activityIntent.putExtra(EXTRA_MESSAGE3,priceText)
+            } else if(price2.isChecked())
+            {
+                activityIntent.putExtra(EXTRA_MESSAGE3, priceText2)
+            } else
+            {
+                activityIntent.putExtra(EXTRA_MESSAGE3, priceText3)
+            }
+            activityIntent.putExtra(EXTRA_MESSAGE, spinnerTxt)
             startActivity(activityIntent)
         }
     }
