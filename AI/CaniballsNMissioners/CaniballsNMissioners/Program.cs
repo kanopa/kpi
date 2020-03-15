0071a2f163cdc97b3762820e0bdfd4f7737f3238
kanopa <konoplyanka1999@gmail.com>
2020-03-15 20:40:27 +0200

    Add folders

---
+++ AI/CaniballsNMissioners/CaniballsNMissioners/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CaniballsNMissioners
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
