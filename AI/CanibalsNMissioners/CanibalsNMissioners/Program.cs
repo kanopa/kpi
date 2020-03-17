d3fbbc3a0775bb8910a069db6ae96d2986ed215c
kanopa <konoplyanka1999@gmail.com>
2020-03-17 20:02:10 +0200

    commit

--- AI/CanibalsNMissioners/CanibalsNMissioners/Program.cs
+++ AI/CanibalsNMissioners/CanibalsNMissioners/Program.cs
@@ -10,10 +10,6 @@ namespace CanibalsNMissioners
             Transition startTransition = new Transition(3, 3, Boat.Left, 0, 0, 0, 0);
             Algorithm algorithm = new Algorithm();
             Transition result = algorithm.IDDFS(startTransition);
-            OutputSolution(result);
-        }
-        static void OutputSolution(Transition result)
-        {
             if (result == null)
                 Console.WriteLine("No solution");
             else
@@ -26,16 +22,37 @@ namespace CanibalsNMissioners
                     transition = transition.parentSolution;
                 }
                 int depth = path.Count - 1;
+                int counter = 0;
                 for (int i = depth; i >= 0; i--)
                 {
                     transition = path[i];
-                    Console.WriteLine("cannibalsLeft:{0}, missionariesLeft: {1}, boat:{2}, cannibalsBoat:{3}, missionariesBoat: {4}, " +
-                                        "cannibalsRight: {5}, missionariesRight: {6} ",
+                    if (transition.boat == Boat.LeftToRight || transition.boat == Boat.RightToLeft)
+                        counter++;
+                    if(transition.boat == Boat.Left || transition.boat == Boat.Right)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine("Cannibals L:{0}, Missioner L: {1},  --- Boat:{2}  Cannibal:{3}  Missioner: {4} ---  " +
+                                        "Cannibal R: {5}, Missioner R: {6} ",
                                         transition.cannibalLeft, transition.missionerLeft, transition.boat, transition.cannibalBoat, transition.missionerBoat, transition.cannibalRight, transition.missionerRight);
-                    Console.WriteLine();
+                        Console.ResetColor();
+                        Console.WriteLine();
+                    }
+                    else
+                    {
+                        Console.WriteLine("Cannibals L:{0}, Missioner L: {1},  --- Boat:{2}  Cannibal:{3}  Missioner: {4} ---  " +
+                                        "Cannibal R: {5}, Missioner R: {6} ",
+                                        transition.cannibalLeft, transition.missionerLeft, transition.boat, transition.cannibalBoat, transition.missionerBoat, transition.cannibalRight, transition.missionerRight);
+                    }
                 }
+                Console.WriteLine("Green color - Near which shore is the boat");
+                Console.WriteLine();
+                Console.ForegroundColor = ConsoleColor.Red;
                 Console.WriteLine("Depth: {0}", depth);
+                Console.WriteLine("Transitions: {0}", counter);
+                Console.WriteLine();
+                Console.ResetColor();
             }
         }
+
     }
 }
