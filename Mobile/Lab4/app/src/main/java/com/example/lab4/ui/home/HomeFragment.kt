84a066fdd871f39400490d38a1c009d9678c24e7
kanopa <konoplyanka1999@gmail.com>
2020-05-26 04:55:25 +0300

    Add 4 lab mobile

--- Mobile/Lab4/app/src/main/java/com/example/lab4/ui/home/HomeFragment.kt
+++ Mobile/Lab4/app/src/main/java/com/example/lab4/ui/home/HomeFragment.kt
@@ -1,18 +1,26 @@
 package com.example.lab4.ui.home
 
-import android.net.Uri
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import android.media.AudioManager
+import android.media.MediaPlayer
+import android.media.MediaPlayer.OnCompletionListener
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import android.widget.MediaController
-import android.widget.VideoView
+import android.widget.SeekBar
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.lab4.R
-import kotlin.math.log
+import kotlinx.android.synthetic.main.fragment_home.*
+
 
 class HomeFragment : Fragment() {
+
+    var mPlayer: MediaPlayer? = null
 
     override fun onCreateView(
             inflater: LayoutInflater,
@@ -20,6 +28,100 @@ class HomeFragment : Fragment() {
             savedInstanceState: Bundle?
     ): View? {
         val root = inflater.inflate(R.layout.fragment_home, container, false)
+        mPlayer=MediaPlayer.create(root.context, R.raw.news);
+        val playMusic = root.findViewById(R.id.button6) as Button
+        val pauseMusic = root.findViewById(R.id.button7) as Button
+        val stopMusic = root.findViewById(R.id.button8) as Button
+        val chooseFile = root.findViewById(R.id.button4) as Button
+        val audioManager = context!!.getSystemService(Context.AUDIO_SERVICE) as AudioManager
+        val maxVolume = audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+        val curValue = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
+        val volume = root.findViewById(R.id.seekBar) as SeekBar
+        mPlayer!!.setOnCompletionListener(OnCompletionListener {
+            stopPlay()
+        })
+        volume.setMax(maxVolume);
+        volume.setProgress(curValue);
+        playMusic.setOnClickListener {
+            play()
+        }
+        pauseMusic.setOnClickListener {
+            pause()
+        }
+        stopMusic.setOnClickListener {
+            stop()
+        }
+        chooseFile.setOnClickListener {
+            chooseFile()
+        }
+        volume.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar, i: Int, b: Boolean) {
+                audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, i, 0);
+            }
+            override fun onStartTrackingTouch(seekBar: SeekBar) {
+            }
+            override fun onStopTrackingTouch(seekBar: SeekBar) {
+            }
+        })
         return root
+    }
+
+    private fun stopPlay() {
+        mPlayer!!.stop()
+        button7.setEnabled(false)
+        button8.setEnabled(false)
+        try {
+            mPlayer!!.prepare()
+            mPlayer!!.seekTo(0)
+            button6.setEnabled(true)
+        } catch (t: Throwable) {
+            Toast.makeText(context, t.message, Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun chooseFile()
+    {
+        val intent = Intent()
+        intent.setType("audio/*")
+        intent.setAction(Intent.ACTION_GET_CONTENT)
+        startActivityForResult(intent, 101)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(resultCode == Activity.RESULT_OK && data != null)
+        {
+            if(requestCode == 101)
+            {
+                val uri = data.data!!
+                button6.setEnabled(true)
+                mPlayer = MediaPlayer.create(context, uri);
+                mPlayer!!.setOnCompletionListener { stopPlay() }
+            }
+        }
+    }
+
+    fun play() {
+        mPlayer!!.start()
+        button6.setEnabled(false)
+        button7.setEnabled(true)
+        button8.setEnabled(true)
+    }
+
+    fun pause() {
+        mPlayer!!.pause()
+        button6.setEnabled(true)
+        button7.setEnabled(false)
+        button8.setEnabled(true)
+    }
+    fun stop() {
+        stopPlay()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        if (mPlayer!!.isPlaying) {
+            stopPlay()
+        }
     }
 }
--- Mobile/Lab4/app/src/main/java/com/example/lab4/ui/notifications/NotificationsFragment.kt
+++ Mobile/Lab4/app/src/main/java/com/example/lab4/ui/notifications/NotificationsFragment.kt
@@ -1,18 +1,18 @@
 package com.example.lab4.ui.notifications
 
+import android.app.Activity
+import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.MediaController
-import android.widget.TextView
-import android.widget.VideoView
+import android.widget.*
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.example.lab4.R
+import kotlinx.android.synthetic.main.fragment_notifications.*
 
 class NotificationsFragment : Fragment() {
 
@@ -23,11 +23,13 @@ class NotificationsFragment : Fragment() {
     ): View? {
         val root = inflater.inflate(R.layout.fragment_notifications, container, false)
         val videoPlayer2: VideoView =  root.findViewById(R.id.videoPlayer2) as VideoView
-        val uri: Uri = Uri.parse("http://www.ebookfrenzy.com/android_book/movie.mp4")
+        val uri: Uri = Uri.parse("https://www.videvo.net/videvo_files/converted/2015_02/preview/Weaver.mp435523.webm")
         videoPlayer2.setVideoURI(uri)
         val pl = root.findViewById(R.id.button1) as Button
         val ps = root.findViewById(R.id.button2) as Button
         val st = root.findViewById(R.id.button3) as Button
+        val choose = root.findViewById(R.id.button) as Button
+        val input = root.findViewById(R.id.button5) as Button
         pl.setOnClickListener {
             start(videoPlayer2);
         }
@@ -36,6 +38,12 @@ class NotificationsFragment : Fragment() {
         }
         st.setOnClickListener {
             stop(videoPlayer2);
+        }
+        choose.setOnClickListener {
+            chooseFile()
+        }
+        input.setOnClickListener {
+            input(root)
         }
         return root
     }
@@ -49,5 +57,33 @@ class NotificationsFragment : Fragment() {
     fun stop(videoPlayer: VideoView){
         videoPlayer.stopPlayback();
         videoPlayer.resume();
+    }
+    fun input(root: View){
+        val mediaController = MediaController(context)
+        val text = root.findViewById(R.id.editText) as EditText
+        videoPlayer2.setMediaController(mediaController)
+        videoPlayer2.setVideoURI(Uri.parse(text.text.toString()))
+    }
+    fun chooseFile(){
+        val intent = Intent()
+        intent.setType("video/*")
+        intent.setAction(Intent.ACTION_GET_CONTENT)
+        startActivityForResult(intent, 101)
+    }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(resultCode == Activity.RESULT_OK && data != null)
+        {
+            if(requestCode == 101)
+            {
+                val mediaController = MediaController(context)
+                videoPlayer2.setMediaController(mediaController)
+                mediaController.setMediaPlayer(videoPlayer2)
+
+
+                val uri = data.data!!
+                videoPlayer2.setVideoURI(uri)
+            }
+        }
     }
 }
